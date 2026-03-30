bb72b5d3a875f3b14532fe0e37876bb98506e116
ajep12 <74065631+ajep12@users.noreply.github.com>
2026-03-30 17:50:06 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css"
+import type { Metadata } from "next"
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Echelon Park Portal",
   description: "Staff Operations System",
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
